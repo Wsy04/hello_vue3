20d7e19828ea310c1cef4372a985c6db09288077
wsy <u202111340@hust.edu.cn>
2024-09-01 15:09:01 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- html结构 -->
+    <div class="app">
+        <h1>你好你好</h1>
+    </div>
+
+</template>
+
+<script>
+    // js或者ts
+    export default{
+        name:'App'
+    }
+</script>
+
+<style>
+    /* 样式 */
+</style>
