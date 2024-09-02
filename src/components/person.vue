86beae99772ad84cf715692ce99ab1448bf09b94
wsy <u202111340@hust.edu.cn>
2024-09-02 16:08:05 +0800

    update

--- src/components/person.vue
+++ src/components/person.vue
@@ -1,35 +1,46 @@
 <template>
     <div class="person">
-        <h2>姓名:{{ name }}</h2>
-        <h2>年龄:{{ age }}</h2>
+        <h2>姓名:{{ man.name }}</h2>
+        <h2>年龄:{{ man.age }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
         <button @click="showTel">查看联系方式</button>
+        <br>
+        <h2>游戏列表</h2>
+        <ul>
+            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+        <button @click="changeFirstName">修改第一个游戏的名字</button>
+        <button @click="changeGame">修改游戏</button>
     </div>
 </template>
 
-<script>
-    export default{
-        name:'Person',
-        setup(){
-            //data
-            let name = '张三'
-            let age = 18
-            let tel = '138888888'
-            
-            //method
-            function changeName(){
-                name = 'zhangsan'
-            }
-            function changeAge(){
-                age+=1
-            }
-            function showTel(){
-                alert(tel)
-            }
-            return {name,age,changeName,changeAge,showTel}
-        }
+<script lang="ts" setup name="person1">
+    import {reactive, ref} from 'vue'
+    //data
+    let man = ref({name:'张三',age:18,tel:'1388888888'})
+    let games = reactive([
+        {id:'aguk01',name:'三国杀'},
+        {id:'aguk02',name:'王者荣耀'},
+        {id:'aguk03',name:'黑神话:悟空'}
+    ])
+    //method
+    function changeName(){
+        man.value.name = 'zhangsan'
     }
+    function changeAge(){
+        man.value.age+=1
+    }
+    function showTel(){
+        alert(man.value.tel)
+    }
+    function changeFirstName(){
+        games[0].name = '原神'
+    }
+    function changeGame(){
+        Object.assign(games[0],{id:'aguk04',name:'柚子社'})
+    }
+
 </script>
 
 <style scoped>
@@ -41,5 +52,8 @@
     }
     button{
         margin: 0 5px;
+    }
+    li{
+        font-size: 20px;
     }
 </style>
